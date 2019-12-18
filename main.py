5ea233b2f806c48eea7dfd628b462a7526a4c066
Victor Munduruca <victormunduruca@gmail.com>
2019-12-18 21:59:29 +0000

    Warp and back working

--- main.py
+++ main.py
@@ -18,10 +18,18 @@ homo, mask = cv2.findHomography(px, m, cv2.RANSAC, 5.0)
 print(homo)
 
 h, w, c = image.shape
-im_dst = cv2.warpPerspective(image, homo, (w, h))
+
+img_homo = cv2.warpPerspective(image, homo, (w, h))
+
+#cv2.line(img_homo, (293, 55), (115, 55), (0,255,0), 2)
+
+#homo, mask = cv2.findHomography(m, px, cv2.RANSAC, 5.0)
+homo_inverse = np.linalg.inv(homo)
+img_straight = cv2.warpPerspective(img_homo, homo_inverse, (w, h))
+
 
 #Segmentation and threshholding
-
+'''
 hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 lower_green = np.array([40, 40, 40])
 upper_green = np.array([70, 255, 255])
@@ -33,7 +41,7 @@ res = cv2.bitwise_and(image, image, mask=mask)
  #convert to hsv to gray
 res_bgr = cv2.cvtColor(res,cv2.COLOR_HSV2BGR)
 res_gray = cv2.cvtColor(res,cv2.COLOR_BGR2GRAY)
-
+'''
 
 #Defining a kernel to do morphological operation in threshold #image to get better output.
 '''
@@ -44,7 +52,8 @@ thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
 cv2.imshow("BGR", image)
 #cv2.imshow("BGR", res_bgr)
 #cv2.imshow("Gray", res_gray)
-cv2.imshow("Warped", im_dst)
+cv2.imshow("Homo", img_homo)
+cv2.imshow("Straight", img_straight)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
