9417849d2fac3fb95383f8886a40b57ed1ae7fe0
Victor Munduruca <victormunduruca@gmail.com>
2019-12-19 14:02:44 +0000

    Partial masked line

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ image = cv2.imread("match.jpg")
 #lines = cv2.HoughLines(edges, 1, np.pi/180, 200)
 
 
+
 m = np.array([[110,0], [110, 55], [293.2, 55], [293.2, 0]]) #fields real coordinates in meters
 px = np.array([[303, 117], [180, 131], [430, 227], [566, 206]]) #pixels coodinates
 
@@ -19,17 +20,21 @@ print(homo)
 
 h, w, c = image.shape
 
-img_homo = cv2.warpPerspective(image, homo, (w, h))
+# Create a black image
+img_blk = np.zeros((512,512,3), np.uint8) #black image to draw line on top
 
-cv2.line(img_homo, (293, 55), (115, 55), (0,255,0), 2)
+img_homo = cv2.warpPerspective(img_blk, homo, (w, h))
 
-#homo, mask = cv2.findHomography(m, px, cv2.RANSAC, 5.0)
+cv2.line(img_homo, (293, 45), (115, 45), (255,255,255), 2)
+
 homo_inverse = np.linalg.inv(homo)
 img_straight = cv2.warpPerspective(img_homo, homo_inverse, (w, h))
 
+cv2.imshow("Black image with line", img_straight)
+
 
 #Segmentation and threshholding
-'''
+
 hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 lower_green = np.array([40, 40, 40])
 upper_green = np.array([70, 255, 255])
@@ -37,11 +42,11 @@ upper_green = np.array([70, 255, 255])
 #Define a mask ranging from lower to uppper
 mask = cv2.inRange(hsv, lower_green, upper_green)
  #Do masking
-res = cv2.bitwise_and(image, image, mask=mask)
+res = cv2.bitwise_and(img_straight, img_straight, mask=mask)
  #convert to hsv to gray
 res_bgr = cv2.cvtColor(res,cv2.COLOR_HSV2BGR)
-res_gray = cv2.cvtColor(res,cv2.COLOR_BGR2GRAY)
-'''
+#res_gray = cv2.cvtColor(res,cv2.COLOR_BGR2GRAY)
+
 
 #Defining a kernel to do morphological operation in threshold #image to get better output.
 '''
@@ -49,11 +54,11 @@ kernel = np.ones((13,13),np.uint8)
 thresh = cv2.threshold(res_gray,127,255,cv2.THRESH_BINARY_INV |  cv2.THRESH_OTSU)[1]
 thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
 '''
-cv2.imshow("BGR", image)
-#cv2.imshow("BGR", res_bgr)
+cv2.imshow("Original image", image)
+cv2.imshow("Image with masked line", res_bgr)
 #cv2.imshow("Gray", res_gray)
-cv2.imshow("Homo", img_homo)
-cv2.imshow("Straight", img_straight)
+#cv2.imshow("Homo", img_homo)
+#cv2.imshow("Straight", img_straight)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
